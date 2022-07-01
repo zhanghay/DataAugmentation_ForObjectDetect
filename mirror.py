16e9c53c8803de3ff535135f1a9f54bb2cab7f8b
zhanghay <2867334718@qq.com>
2022-07-01 22:06:49 +0800

    支持json和bbox和point

--- mirror.py
+++ mirror.py
@@ -5,137 +5,173 @@ import os
 import glob
 import json
 import shutil
-import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import ElementTree, Element
 
-def h_MirrorImg(img_path,img_write_path):
+
+# 翻转图片后保存图片
+# h 水平
+# v 垂直
+# a 水平+垂直
+def h_MirrorImg(img_path, img_write_path):
     img = cv2.imread(img_path)
-    mirror_img = cv2.flip(img, 1)  #
-    cv2.imwrite(img_write_path,mirror_img)
-def v_MirrorImg(img_path,img_write_path):
+    mirror_img = cv2.flip(img, 1)  # 1 水平翻转， 0 垂直 -1 水平+垂直
+    cv2.imwrite(img_write_path, mirror_img)
+
+
+def v_MirrorImg(img_path, img_write_path):
     img = cv2.imread(img_path)
     mirror_img = cv2.flip(img, 0)  #
-    cv2.imwrite(img_write_path,mirror_img)
-def a_MirrorImg(img_path,img_write_path):
+    cv2.imwrite(img_write_path, mirror_img)
+
+
+def a_MirrorImg(img_path, img_write_path):
     img = cv2.imread(img_path)
     mirror_img = cv2.flip(img, -1)  #
-    cv2.imwrite(img_write_path,mirror_img)
+    cv2.imwrite(img_write_path, mirror_img)
 
-def h_MirrorAnno(anno_path,anno_write_path):
-    tree = ET.parse(anno_path)
-    root = tree.getroot()
-    size=root.find('size')
-    w=int(size.find('width').text)
-    objects = root.findall("object")
-    for obj in objects:
-        bbox = obj.find('bndbox')
-        x1 = float(bbox.find('xmin').text)
-        x2 = float(bbox.find('xmax').text)
-        x1=w-x1+1
-        x2=w-x2+1
 
-        assert x1>0
-        assert x2>0
+# 翻转注释
+def h_MirrorAnno(anno_path, anno_write_path):
+    with open(anno_path, 'r', encoding='utf8') as js:
+        data = json.load(js)
+        js.close()
+    bbox = data['shapes'][0]['points']
 
-        bbox.find('xmin').text=str(int(x2))
-        bbox.find('xmax').text=str(int(x1))
+    x1 = bbox[0][0]
+    x2 = bbox[1][0]
+    w = data['imageWidth']
 
-    tree.write(anno_write_path)  # 保存修改后的XML文件
-def v_MirrorAnno(anno_path,anno_write_path):
-    tree = ET.parse(anno_path)
-    root = tree.getroot()
-    size = root.find('size')
-    h=int(size.find('height').text)
-    objects = root.findall("object")
-    for obj in objects:
-        bbox = obj.find('bndbox')
-        y1 = float(bbox.find('ymin').text)
-        y2 = float(bbox.find('ymax').text)
+    x1 = w - x1 + 1  # 坐标操作
+    x2 = w - x2 + 1
 
-        y1=h-y1+1
-        y2=h-y2+1
+    assert x1 > 0  # 非负数
+    assert x2 > 0
 
-        assert y1>0
-        assert y2>0
+    bbox[0][0] = int(x2)
+    bbox[1][0] = int(x1)
 
-        bbox.find('ymin').text=str(int(y2))
-        bbox.find('ymax').text=str(int(y1))
+    for i in range(8):
+        point_x = data['shapes'][i + 1]['points'][0][0]
+        point_x = w - point_x + 1
+        assert point_x > 0
+        data['shapes'][i + 1]['points'][0][0] = point_x
+        
+    with open(anno_write_path, "w") as f:
+        json.dump(data, f)
+        f.close()
 
-    tree.write(anno_write_path)  # 保存修改后的XML文件
-def a_MirrorAnno(anno_path,anno_write_path):
-    tree = ET.parse(anno_path)
-    root = tree.getroot()
-    size = root.find('size')
-    w=int(size.find('width').text)
-    h = int(size.find('height').text)
-    objects = root.findall("object")
-    for obj in objects:
-        bbox = obj.find('bndbox')
-        x1 = float(bbox.find('xmin').text)
-        y1 = float(bbox.find('ymin').text)
-        x2 = float(bbox.find('xmax').text)
-        y2 = float(bbox.find('ymax').text)
 
-        x1=w-x1+1
-        x2=w-x2+1
+def v_MirrorAnno(anno_path, anno_write_path):
+    with open(anno_path, 'r', encoding='utf8') as js:
+        data = json.load(js)
+        js.close()
+    bbox = data['shapes'][0]['points']
 
-        y1 = h - y1+1
-        y2 = h - y2+1
+    y1 = bbox[0][1]
+    y2 = bbox[1][1]
+    h = data['imageHeight']
 
-        assert x1 > 0
-        assert x2 > 0
-        assert y1 > 0
-        assert y2 > 0
+    y1 = h - y1 + 1  # 坐标操作
+    y2 = h - y2 + 1
 
-        bbox.find('xmin').text=str(int(x2))
-        bbox.find('xmax').text=str(int(x1))
-        bbox.find('ymin').text=str(int(y2))
-        bbox.find('ymax').text=str(int(y1))
+    assert y1 > 0  # 非负数
+    assert y2 > 0
+    bbox[0][1] = int(y2)
+    bbox[1][1] = int(y1)
 
-    tree.write(anno_write_path)  # 保存修改后的XML文件
-def mirror(img_dir,anno_dir,img_write_dir,anno_write_dir):
+    for i in range(8):
+        point_y = data['shapes'][i + 1]['points'][0][1]
+        point_y = h - point_y + 1
+        assert point_y > 0
+        data['shapes'][i + 1]['points'][0][1] = point_y
+
+
+    with open(anno_write_path, "w") as f:
+        json.dump(data, f)
+        f.close()
+
+
+def a_MirrorAnno(anno_path, anno_write_path):
+    with open(anno_path, 'r', encoding='utf8') as js:
+        data = json.load(js)
+        js.close()
+    bbox = data['shapes'][0]['points']
+
+    x1 = bbox[0][0]
+    x2 = bbox[1][0]
+    y1 = bbox[0][1]
+    y2 = bbox[1][1]
+    h = data['imageHeight']
+    w = data['imageWidth']
+
+    x1 = w - x1 + 1
+    x2 = w - x2 + 1
+
+    y1 = h - y1 + 1
+    y2 = h - y2 + 1
+
+    assert x1 > 0
+    assert x2 > 0
+    assert y1 > 0
+    assert y2 > 0
+
+    bbox[0][0] = int(x2)
+    bbox[1][0] = int(x1)
+    bbox[0][1] = int(y2)
+    bbox[1][1] = int(y1)
+
+    for i in range(8):
+
+        point_x = data['shapes'][i + 1]['points'][0][0]
+        point_x = w - point_x + 1
+        assert point_x > 0
+        data['shapes'][i + 1]['points'][0][0] = point_x
+
+        point_y = data['shapes'][i + 1]['points'][0][1]
+        point_y = h - point_y + 1
+        assert point_y > 0
+        data['shapes'][i + 1]['points'][0][1] = point_y
+
+    with open(anno_write_path, "w") as f:
+        json.dump(data, f)
+        f.close()
+
+
+def mirror(img_dir, anno_dir, img_write_dir, anno_write_dir):
     if not os.path.exists(img_write_dir):
         os.makedirs(img_write_dir)
 
     if not os.path.exists(anno_write_dir):
         os.makedirs(anno_write_dir)
-    img_names=os.listdir(img_dir)
+
+    img_names = os.listdir(img_dir)
+    # 所有图片文件
     for img_name in img_names:
-        img_path=os.path.join(img_dir,img_name)
-        h_img_write_path=os.path.join(img_write_dir,img_name[:-4]+'h'+'.jpg')
-        anno_path=os.path.join(anno_dir,img_name[:-4]+'.xml')
-        h_anno_write_path = os.path.join(anno_write_dir, img_name[:-4]+'h'+'.xml')
-        #
-        v_img_write_path = os.path.join(img_write_dir, img_name[:-4] + 'v' + '.jpg')
-        v_anno_write_path = os.path.join(anno_write_dir, img_name[:-4] + 'v' + '.xml')
-        #
-        a_img_write_path = os.path.join(img_write_dir, img_name[:-4] + 'a' + '.jpg')
-        a_anno_write_path = os.path.join(anno_write_dir, img_name[:-4] + 'a' + '.xml')
-        #
-        h_MirrorImg(img_path,h_img_write_path)
-        v_MirrorImg(img_path,v_img_write_path)
-        a_MirrorImg(img_path, a_img_write_path)
-        h_MirrorAnno(anno_path,h_anno_write_path)
-        v_MirrorAnno(anno_path, v_anno_write_path)
-        a_MirrorAnno(anno_path, a_anno_write_path)
+        if img_name.split('.')[-1] == 'jpg' or img_name.split('.')[-1] == 'png':
+            img_path = os.path.join(img_dir, img_name)
+            # 重命名
+            h_img_write_path = os.path.join(img_write_dir, img_name.split('.')[0] + '_h' + '.jpg')
+            anno_path = os.path.join(anno_dir, img_name.split('.')[0] + '.json')
+            h_anno_write_path = os.path.join(anno_write_dir, img_name.split('.')[0] + '_h' + '.json')
+            #
+            v_img_write_path = os.path.join(img_write_dir, img_name.split('.')[0] + '_v' + '.jpg')
+            v_anno_write_path = os.path.join(anno_write_dir, img_name.split('.')[0] + '_v' + '.json')
+            #
+            a_img_write_path = os.path.join(img_write_dir, img_name.split('.')[0] + '_a' + '.jpg')
+            a_anno_write_path = os.path.join(anno_write_dir, img_name.split('.')[0] + '_a' + '.json')
+            #
+            h_MirrorImg(img_path, h_img_write_path)
+            v_MirrorImg(img_path, v_img_write_path)
+            a_MirrorImg(img_path, a_img_write_path)
+            h_MirrorAnno(anno_path, h_anno_write_path)
+            v_MirrorAnno(anno_path, v_anno_write_path)
+            a_MirrorAnno(anno_path, a_anno_write_path)
 
 
-img_dir='several/JPEGImages'
-anno_dir='several/Annotations'
-img_write_dir='Mirror/mirrored_JPEGImages'
-anno_write_dir='Mirror/mirrored_Annotations'
-
-mirror(img_dir,anno_dir,img_write_dir,anno_write_dir)
-'''
-imga=cv2.imread('14a.jpg')
-imgh=cv2.imread('14h.jpg')
-imgv=cv2.imread('14v.jpg')
+if __name__ == '__main__':
+    img_dir = '.'
+    anno_dir = '.'
+    img_write_dir = '.'
+    anno_write_dir = '.'
+    mirror(img_dir, anno_dir, img_write_dir, anno_write_dir)
 
 
-cv2.rectangle(imga,(606,898),(855,1432),(0,255,0),4)
-cv2.rectangle(imgh,(606,489),(855,1023),(0,255,0),4)
-cv2.rectangle(imgv,(32,898),(281,1432),(0,255,0),4)
-cv2.imwrite('a.jpg',imga)
-cv2.imwrite('v.jpg',imgv)
-cv2.imwrite('h.jpg',imgh)
-'''
---
+++ showImgFromJson.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+# author: zhanghay
+# Create Time: 2022/7/1  17:51
+# File Name: showImgFromJson.py
+# Description: 根据json文件画框和点
+#              json判断对不对
+
+import cv2 as cv
+import numpy as np
+import os
+import json
+import mirror
+if __name__ == '__main__':
+    img = cv.imread('003_v.jpg')
+    with open('003_v.json', 'r', encoding='utf8') as js:
+        data = json.load(js)
+        js.close()
+    # 横着是imageWidth
+    # 竖着是imageHeight
+
+    for i in range(8):
+        point = (int(data['shapes'][i + 1]['points'][0][0]), int(data['shapes'][i + 1]['points'][0][1]))
+        cv.circle(img, point, 5, (255, 255, 255), -1)
+    # 点  (int(data['shapes'][1]['points'][0][0]),int(data['shapes'][1]['points'][0][1])) # x , y
+
+    bboxPoint1 = (int(data['shapes'][0]['points'][0][0]), int(data['shapes'][0]['points'][0][1]))
+    # bbox 左上
+    bboxPoint2 = (int(data['shapes'][0]['points'][1][0]), int(data['shapes'][0]['points'][1][1]))
+    # bbox 右下
+    cv.rectangle(img, bboxPoint1, bboxPoint2, (0, 255, 255), 4)
+    cv.imshow('img', img)
+    key = cv.waitKey(0)
+    if key == "a":
+        cv.destroyAllWindows()
